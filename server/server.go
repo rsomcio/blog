d88ea1af9e9270fb9a82bd8b11f609c27c27e477
Ray Somcio <rsomcio@gmail.com>
2020-12-29 05:43:28 +0000

    added cleaner shutdown of server

--- server/server.go
+++ server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	"github.com/rsomcio/blog/blogpb"
 	"google.golang.org/grpc"
@@ -14,8 +16,8 @@ type server struct{}
 
 func (*server) Blog(ctx context.Context, req *blogpb.BlogRequest) (*blogpb.BlogResponse, error) {
 	fmt.Printf("Blog function was invoked with %v\n", req)
-	authorId := req.GetBlog().GetAuthorId()
-	result := "Hello " + authorId
+	authorID := req.GetBlog().GetAuthorId()
+	result := "Hello " + authorID
 	res := &blogpb.BlogResponse{
 		Result: result,
 	}
@@ -23,7 +25,9 @@ func (*server) Blog(ctx context.Context, req *blogpb.BlogRequest) (*blogpb.BlogR
 }
 
 func main() {
-	fmt.Println("Hello World!")
+	// if we crash the co code, we get the file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	fmt.Println("Blog Server Started.")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -33,7 +37,22 @@ func main() {
 	s := grpc.NewServer()
 	blogpb.RegisterBlogServiceServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
+	go func() {
+		fmt.Println("Staring Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the listener")
+	lis.Close()
+	fmt.Println("End of Program")
 }
